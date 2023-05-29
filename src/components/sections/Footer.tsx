93c350c2e21c2d3c0f6628f2c33a18634c352d05
Subhayu <s7chak@gmail.com>
2023-05-28 18:20:16 -0600

    Same main 2

---
+++ src/components/sections/Footer.tsx
@@ -0,0 +1,13 @@
+export const Footer = () => {
+	return (
+		<footer id="footer" className="footer">
+			<div className="content has-text-centered">
+				<p>
+					{/* Copyright © 2023-2026 s7chak. All Rights Reserved. */}
+				</p>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
