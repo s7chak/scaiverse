3d6e5fe323038885954baa105f2939577cc8d88c
s7chak <s7chak@gmail.com>
2024-03-29 22:40:17 -0600

    Scroll Top Blog post

--- src/components/SCBlogCard.tsx
+++ src/components/SCBlogCard.tsx
@@ -3,70 +3,93 @@ import blogConfigData from "./config/blogConfig.json";
 import myBlogConfigData from "./config/scaiBlogConfig.json";
 import React, { useState } from "react";
 
-
-export const SCBlogCard = ({ id, onClick}: { id: string; onClick: () => void; }) => {
-	const [width, setWidth] = useState<number>(window.innerWidth);
-	const isMobile = width <= 768;
-	const blog = myBlogConfigData[id];
-	const title = blog.title;
-	const date = blog.date;
-	const img = blog.img;
-	const handleClick = () => {
-        onClick();
-    };
-	return (
-		<div className={`scblog-card ${isMobile ? "mobile" : ""}`} onClick={handleClick}>
-			<div className="scblog-card-content">
-				<div className='scblog-card-bar'>
-					<div className="scblog-title">{title}</div>
-					<div className="scblog-date">{date}</div>
-				</div>
-				{/* <div className="scblog-img">{img}</div> */}
-				{img && <img src={img} alt={title} className="scblog-img" />}
-			</div>
-		</div>
-	);
-}
-
-export const SCBlogPost = ({ id, onClick }: { id: string; onClick}) => {
-	const config = myBlogConfigData;
-	const [width, setWidth] = useState<number>(window.innerWidth);
-	const isMobile = width <= 768;
-	let blog = config[id];
-	let blogimgs = blog? blog["blogimgs"] : [];
-	if (blogimgs.length>0){
-		let description = blog['description'];
-		const descriptionWithImages: string = description.replace("{img}", blogimgs.map(img => `<img src="${img}">`).join("<br/>"));
-		blog['description'] = descriptionWithImages;
-	}
-	
-	return (
-		<div className={`scblog-post ${isMobile ? "mobile" : ""}`}>
-			<div className="scblog-post-content">
-				{!!blog ? (
-					<>
-						<div className="scpost-title">
-							{blog['title']}
-						</div>
-						<div className="scpost-date">
-							{blog['date']}
-						</div>
-						<div className="scpost-description" dangerouslySetInnerHTML={{ __html: blog['description'] }}>
-						</div>
-						<p><br/><br/></p>
-						<div className='scblogheader'>
-							<div className='gbutton' onClick={onClick}>
-								Back to blogs
-							</div>
-						</div>
-					</>
-					) : null
-				}
-				<br/><br/>
-			</div>
-		</div>
-	);
+export const SCBlogCard = ({
+  id,
+  onClick,
+}: {
+  id: string;
+  onClick: () => void;
+}) => {
+  const [width, setWidth] = useState<number>(window.innerWidth);
+  const isMobile = width <= 768;
+  const blog = myBlogConfigData[id];
+  const title = blog.title;
+  const date = blog.date;
+  const img = blog.img;
+  const handleClick = () => {
+    onClick();
+  };
+  return (
+    <div
+      className={`scblog-card ${isMobile ? "mobile" : ""}`}
+      onClick={handleClick}
+    >
+      <div className="scblog-card-content">
+        <div className="scblog-card-bar">
+          <div className="scblog-title">{title}</div>
+          <div className="scblog-date">{date}</div>
+        </div>
+        {/* <div className="scblog-img">{img}</div> */}
+        {img && <img src={img} alt={title} className="scblog-img" />}
+      </div>
+    </div>
+  );
 };
 
+export const SCBlogPost = ({ id, onClick }: { id: string; onClick }) => {
+  const config = myBlogConfigData;
+  const [width, setWidth] = useState<number>(window.innerWidth);
+  const isMobile = width <= 768;
+  let blog = config[id];
+  // if (!!blog) {
+  // 	fetch(`../config/${id}.html`)
+  // 	  .then((response) => response.text())
+  // 	  .then((html) => {
+  // 		blog.description = html;
+  // 		console.log(id, blog.description);
+  // 	  })
+  // 	  .catch((error) => {
+  // 		blog.description = "<p>Apologies, we'll be back soon.</p>";
+  // 		console.error("Error fetching HTML:", error);
+  // 	  });
+  //   }
+  let blogimgs = blog ? blog["blogimgs"] : [];
+  if (blogimgs.length > 0) {
+    let description = blog["description"];
+    const descriptionWithImages: string = description.replace(
+      "{img}",
+      blogimgs.map((img) => `<img src="${img}">`).join("<br/>")
+    );
+    blog["description"] = descriptionWithImages;
+  }
+
+  return (
+    <div className={`scblog-post ${isMobile ? "mobile" : ""}`}>
+      <div className="scblog-post-content">
+        {!!blog ? (
+          <>
+            <div className="scpost-title">{blog["title"]}</div>
+            <div className="scpost-date">{blog["date"]}</div>
+            <div
+              className="scpost-description"
+              dangerouslySetInnerHTML={{ __html: blog["description"] }}
+            ></div>
+            <p>
+              <br />
+              <br />
+            </p>
+            <div className="scblogheader">
+              <div className="gbutton" onClick={onClick}>
+                Back to blogs
+              </div>
+            </div>
+          </>
+        ) : null}
+        <br />
+        <br />
+      </div>
+    </div>
+  );
+};
 
 export default SCBlogCard;
--- src/pages/Blogger.tsx
+++ src/pages/Blogger.tsx
@@ -9,85 +9,116 @@ import { SCBlogCard, SCBlogPost } from "../components/SCBlogCard";
 import myBlogConfigData from "../components/config/scaiBlogConfig.json";
 
 function FadeInWhenVisible({ children }) {
-	const [ref, inView] = useInView({
-		triggerOnce: false,
-	  });
-	return (
-		<motion.div
-		ref={ref}
-        initial={{ opacity: 0 , y: 30}}
-        animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20}}
-        transition={{ duration: 2 }}
-		>
-		{children}
-	  </motion.div>
-	);
+  const [ref, inView] = useInView({
+    triggerOnce: false,
+  });
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
+      transition={{ duration: 2 }}
+    >
+      {children}
+    </motion.div>
+  );
 }
 
-
 export const Blogger = () => {
-	const [theme, setTheme] = useState("dark");
-	const [width, setWidth] = useState<number>(window.innerWidth);
-	const [postId, setPostId] = useState<string>('');
-	const [prevPostId, setPrevPostId] = useState('');
-	const isMobile = width <= 768;
-	const blogCardsRef = useRef(null);
-	let config = myBlogConfigData;
-	const getBlogTitles = (): any => {
-		let extractedData = {};
-		for (const blogId in config) {
-			if (Object.hasOwnProperty.call(config, blogId)) {
-				const { title, img } = config[blogId];
-				extractedData[blogId] = { title, img };
-			}
-		}
-		return extractedData;
-	};
-	const [showPostContainer, setShowPostContainer] = useState(false);
-	const scblogcards = getBlogTitles();
-	const setPost = (id, on) => {
-		setShowPostContainer(on);
-		setPostId(id);
-		scrollToTop();
-	};
-	const scrollToTop = () => {
-		const postContainer = document.querySelector('.scblog-post');
-		if (postContainer) {
-			postContainer.scrollIntoView({ behavior: 'smooth', block: 'start' });
-		}
-	};
-	return (
-			<div id={theme} className="scblogging is-medium">
-				<div className="main-container">
-					<div className="scblogheader">
-						<motion.div whileInView={{ opacity: 1}} initial={{ opacity: 0}} animate={{ opacity: 1}} transition={{ease: "linear", duration: 1.5}}>
-							<span className="general-header" onClick={() => setShowPostContainer(false)}>Insight Room</span>
-						</motion.div>
-						<br/>
-						<motion.div initial={{ opacity: 0, scale: 0.94}} animate={{ opacity: 1, scale:1}} transition={{ease: "linear", duration: 2.5}}>
-							<span className={`scblogheadertagline ${isMobile ? "mobile" : ""}`}>
-								A repository of thoughts, ideas and insight.</span><br/><br/>
-						</motion.div>
-						<motion.div className='scblog-scbutton' initial={{ opacity: 0}} animate={{ opacity: 1}} transition={{ease: "linear", duration: 3}}>
-							<Link to={"/"}>
-								<div className="gbutton">
-									Scaiverse
-								</div>
-							</Link>
-						</motion.div>
-					</div>
-				</div>
-				<div className={`post-container ${showPostContainer ? 'visible' : ''}`}>
-					<SCBlogPost id={postId} onClick={() => { setShowPostContainer(false);}} />
-				</div>
-				<div className={`scblog-cards ${isMobile ? "mobile" : ""}`} ref={blogCardsRef}>
-					{Object.keys(scblogcards).map((key) => (
-						<SCBlogCard id={key} onClick = {() => {setPost(key, true);}} />
-					))}
-				</div>
-				
-			</div>
-	);
+  const [theme, setTheme] = useState("dark");
+  const [width, setWidth] = useState<number>(window.innerWidth);
+  const [postId, setPostId] = useState<string>("");
+  const [prevPostId, setPrevPostId] = useState("");
+  const isMobile = width <= 768;
+  const blogCardsRef = useRef(null);
+  let config = myBlogConfigData;
+  const getBlogTitles = (): any => {
+    let extractedData = {};
+    for (const blogId in config) {
+      if (Object.hasOwnProperty.call(config, blogId)) {
+        const { title, img } = config[blogId];
+        extractedData[blogId] = { title, img };
+      }
+    }
+    return extractedData;
+  };
+  const [showPostContainer, setShowPostContainer] = useState(false);
+  const scblogcards = getBlogTitles();
+  const setPost = (id, on) => {
+    scrollToTop();
+    setShowPostContainer(on);
+    setPostId(id);
+  };
+  const scrollToTop = () => {
+    const postContainer = document.querySelector(".scblog-post");
+    if (postContainer) {
+      postContainer.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+  return (
+    <div id={theme} className="scblogging is-medium">
+      <div className="main-container">
+        <div className="scblogheader">
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ ease: "linear", duration: 1.5 }}
+          >
+            <span
+              className="general-header"
+              onClick={() => setShowPostContainer(false)}
+            >
+              Insight Room
+            </span>
+          </motion.div>
+          <br />
+          <motion.div
+            initial={{ opacity: 0, scale: 0.94 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ ease: "linear", duration: 2.5 }}
+          >
+            <span className={`scblogheadertagline ${isMobile ? "mobile" : ""}`}>
+              A repository of thoughts, ideas and insight.
+            </span>
+            <br />
+            <br />
+          </motion.div>
+          <motion.div
+            className="scblog-scbutton"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ ease: "linear", duration: 3 }}
+          >
+            <Link to={"/"}>
+              <div className="gbutton">Scaiverse</div>
+            </Link>
+          </motion.div>
+        </div>
+      </div>
+      <div className={`post-container ${showPostContainer ? "visible" : ""}`}>
+        <SCBlogPost
+          id={postId}
+          onClick={() => {
+            setShowPostContainer(false);
+          }}
+        />
+      </div>
+      <div
+        className={`scblog-cards ${isMobile ? "mobile" : ""}`}
+        ref={blogCardsRef}
+      >
+        {Object.keys(scblogcards).map((key) => (
+          <SCBlogCard
+            id={key}
+            onClick={() => {
+              setPost(key, true);
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Blogger;
