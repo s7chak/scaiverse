f49446305c81001d43941f12b0a0f4ef30b2ad7c
s7chak <s7chak@gmail.com>
2024-04-29 18:49:24 -0600

    Naming

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import { Link } from "react-scroll";
 
 export const Footer = () => {
-	return (
-		<footer id="footer" className="footer">
-			<Link smooth={true} to="introduction" duration={3500}><span className="general-header">Cheers!</span></Link>
-			<br/><br/>
-			<span className="footertext">
-				Made with React JS, Fiber, Three, DREI, Framer Motion, HTML5, SCSS.<br/>
-				Copyright © 2023-2026 Subhayu Chakravarty.
-				<p>All Rights Reserved.</p>
-			</span>
-		</footer>
-	);
+  return (
+    <footer id="footer" className="footer">
+      <Link smooth={true} to="introduction" duration={3500}>
+        <span className="general-header">Cheers!</span>
+      </Link>
+      <br />
+      <br />
+      <span className="footertext">
+        Made with React JS, Fiber, Three, DREI, Framer Motion, HTML5, SCSS.
+        <br />
+        Copyright © S C.
+        <p>All Rights Reserved.</p>
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
